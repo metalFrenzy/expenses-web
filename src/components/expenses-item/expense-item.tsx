2138479c0fcaa96aab85a3fa167a05fea9cad44b
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2024-10-31 16:55:34 +0300

    setting up done

---
+++ src/components/expenses-item/expense-item.tsx
@@ -0,0 +1,10 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>
+                <h2>This is zobryyy</h2>
+            </div>
+        </div>
+    )
+}
+export default ExpenseItem;
