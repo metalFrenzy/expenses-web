e363213398a2a98e2bc625f304e639900bd4b8fa
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-01-07 01:46:27 +0200

    playing with use states

--- src/components/expense-category/expense-category.tsx
+++ src/components/expense-category/expense-category.tsx
@@ -1,5 +1,6 @@
 import './expense-categor.scss';
 import shoppingCartIcon from "../../assets/shopping-cart.svg";
+import { useState } from 'react';
 
 
 type Props = {
@@ -11,12 +12,17 @@ type Props = {
   };
 
 function ExpenseCategory({ category }: Props) {
+  const [title, setTitle] = useState(category.name);
+  const clickHandler = () =>{
+    setTitle('New Title');
+  }
     return <div className='expense-category'>
-        <h3>{category.name}</h3>
+        <h3>{title}</h3>
          <div className='img-container'> 
             <img src={shoppingCartIcon} alt="Shopping Cart Icon" />
          </div> 
         <h4>{category.amount}</h4>
+        <button onClick={clickHandler}>Add Expense</button>
     </div>
 }
 
