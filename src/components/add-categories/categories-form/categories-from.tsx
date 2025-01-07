912747e31cd55b41f9141fe326f2f76fb36245be
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-01-07 23:03:29 +0200

    feat(ui): add categories form

---
+++ src/components/add-categories/categories-form/categories-from.tsx
@@ -0,0 +1,45 @@
+import './categories-form.scss'
+import { useState } from 'react'
+
+interface Props {
+    onSave: (categoryData: { title: string; amount: string }) => void;
+}
+
+function CategoriesForm({onSave}: Props) {
+    const [title, setTitle] = useState('')
+    const [amount, setAmount] = useState('')
+    const titleChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.target.value)
+    }
+    const amountChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setAmount(event.target.value)
+    }
+
+    const submitHandler = (event: React.FormEvent) => {
+        event.preventDefault()
+        const catgoryData = {
+            title:title,
+            amount: amount
+        }
+        onSave(catgoryData)
+        setTitle('');
+        setAmount('');
+    }
+    return <form onSubmit={submitHandler}>
+        <div className="catgories-form">
+            <div className="title">
+                <label>Title</label>
+                <input type="text" onChange={titleChangeHandler} value={title} />
+            </div>
+            <div className="ammount">
+                <label>Amount</label>
+                <input type="number" onChange={amountChangeHandler} value={amount} />
+            </div>
+        </div>
+        <div className="action-button">
+            <button className="add" type='submit'>Add Category</button>
+        </div>
+    </form>
+}
+
+export default CategoriesForm
