d9b117f41dbd469f55afbccfd6d2134258718ffa
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-01-07 23:03:48 +0200

    feat(ui): saving the value to the state

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,8 +1,13 @@
+import AddCategories from "../../components/add-categories/add-new-categories";
 import AddCategoryButton from "../../components/add-category-button";
 import HeadingSection from "../../components/heading-section";
 import './home-page.scss'
 
 export default function HomePage() {
+
+    const addCategory = (catgories: { title: string; amount: string, id: string}) => {
+        console.log(catgories)
+    }
 
     return <>
         <div heading-section>
@@ -10,6 +15,7 @@ export default function HomePage() {
         </div>
 
         <AddCategoryButton></AddCategoryButton>
-        
+        <AddCategories onAdd={addCategory}></AddCategories>
+
     </>
 }
