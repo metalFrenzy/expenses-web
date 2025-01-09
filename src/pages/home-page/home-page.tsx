0b3a28beadfe8bb170ac81755897f25c603dfd7a
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-01-09 22:30:19 +0200

    feat(ui): updating the added items to the list using usestate

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,12 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import AddCategories from "../../components/add-categories/add-new-categories";
 import AddCategoryButton from "../../components/add-category-button";
 import HeadingSection from "../../components/heading-section";
+import { useState } from "react";
 import './home-page.scss'
+
+const expensCatgories = [
+    {
+        id: "1",
+        title: "Food & Drinks",
+        amount: "1500EGP",
+    },
+    {
+        id: "1",
+        title: "Shopping",
+        amount: "5000EGP",
+    }
+]
 
 export default function HomePage() {
 
-    const addCategory = (catgories: { title: string; amount: string, id: string}) => {
-        console.log(catgories)
+    const [categories, setCategories] = useState(expensCatgories)
+
+    const addCategory = (catgories: { title: string; amount: string, id: string }) => {
+        console.log('this is the cats from the home',catgories)
+        setCategories((prevState) => {
+            return [...prevState,catgories]
+        })
     }
 
     return <>
@@ -14,7 +34,7 @@ export default function HomePage() {
             <HeadingSection></HeadingSection>
         </div>
 
-        <AddCategoryButton></AddCategoryButton>
+        <AddCategoryButton expensCatgories={categories}></AddCategoryButton>
         <AddCategories onAdd={addCategory}></AddCategories>
 
     </>
